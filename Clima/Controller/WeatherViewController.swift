06ef2404e844407f5d0dd120b5ed2e1a83ef6f15
Fabio <fabio@MacBook-Air-de-Fabio.local>
2021-10-08 16:33:07 -0300

    first commit

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,58 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+    func didUpdateWeather(weather: WeatherModel?)->WeatherModel {
+        print("llego al delegate")
+        print(weather!)
+        return weather!
+    }
+    func didFailedWithError(error:Error){
+        print(error)
+    }
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    var weatherManager = WeatherManager()
     
+ 
+    @IBOutlet weak var searchTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        weatherManager.delegate = self
+//        con la linea de abajo le decimos a iOS que el control vuelva al controller, luego del keyboard, o sea que responda a este controller
+//        el control en el textfield le dice heyy viewcontroller, por ej, el usuario empezo a teclear
+        searchTextField.delegate = self
     }
 
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    //    la func de abajo habilita al delegate, o sea a esta clase, que reciba el evento de que pasa cuando el usuario apreta la tecla return en el cel, que en realidad aca en el proyecto le pusimos la tecla go, que esta en el mismo lugar que el return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+//    "hey, viewcontroller,ya termine de editar... que hago"?
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text{
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = ""
+    }
+    
+//    "hey, viewcontroller, el usuario se fue del campo e hice tap en otro lado, que hago?"
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
 }
 
---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Fabio on 04/10/2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable{
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+struct Main: Decodable{
+    let temp: Double
+}
+struct Weather: Decodable{
+    let id: Int
+    let description: String
+}
